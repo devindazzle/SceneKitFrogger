d96885d6d8fd185450907c3a612fe20e75f0eae2
Kim Pedersen <dretox@gmail.com>
2014-12-02 12:22:38 +0100

    Added starter project

--- 1-Starter/SCNFrogger/GameScene.swift
+++ 1-Starter/SCNFrogger/GameScene.swift
@@ -1,8 +1,8 @@
 //
 //  GameScene.swift
-//  RWDevConSceneKitFinal
+//  SCNFrogger
 //
-//  Created by Kim Pedersen on 24/11/14.
+//  Created by Kim Pedersen on 02/12/14.
 //  Copyright (c) 2014 RWDevCon. All rights reserved.
 //
 
@@ -11,25 +11,26 @@ import SpriteKit
 
 
 enum GameState {
-case WaitingForFirstTap
-case Playing
-case GameOver
-case RestartLevel
+  case WaitingForFirstTap
+  case Playing
+  case GameOver
+  case RestartLevel
 }
 
 
 enum MoveDirection {
-case Forward
-case Backward
-case Left
-case Right
+  case Forward
+  case Backward
+  case Left
+  case Right
 }
 
+
 struct PhysicsCategory {
-static let None: Int              = 0
-static let Player: Int            = 0b1      // 1
-static let Car: Int               = 0b10     // 2
-static let Obstacle: Int          = 0b100    // 4
+  static let None: Int              = 0
+  static let Player: Int            = 0b1      // 1
+  static let Car: Int               = 0b10     // 2
+  static let Obstacle: Int          = 0b100    // 4
 }
 
 
@@ -42,24 +43,9 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   var cameraOrthographicScale = 0.5
   var cameraOffsetFromPlayer = SCNVector3(x: 0.25, y: 1.25, z: 0.55)
   var level: SCNNode!
-  var player: SCNNode!
-  var playerModelNode: SCNNode!
-  var playerGridCol = 7
-  var playerGridRow = 6
-  var previousTime: NSTimeInterval = 0
-  var deltaTime: NSTimeInterval = 0
   var levelData: GameLevel!
-  var sharedCarMaterial: SCNMaterial!
-  let soundJump = SKAction.playSoundFileNamed("assets.scnassets/Sounds/jump.wav", waitForCompletion: false)
   
-  /* FOR LAB SESSION */
-  let playerScene = SCNScene(named: "assets.scnassets/Models/frog.dae")
-  var sharedMaterial: SCNMaterial!
-  /* FOR LAB SESSION */
-  
-  /* FOR CHALLENGE SESSION */
-  let carScene = SCNScene(named: "assets.scnassets/Models/car.dae")
-  /* FOR CHALLENGE SESSION */
+  // TODO: Add player properties here
   
   
   init(view: SCNView) {
@@ -76,12 +62,12 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   
   
   func initializeLevel() {
+    
     // Set up gesture recognizers
     setupGestureRecognizersForView(view)
     
     // Create level procedurally
     levelData = GameLevel(width: 19, height: 50)
-    println("\(levelData.description)")
     
     // Create ambient light
     let ambientLight = createAmbientLight()
@@ -91,30 +77,19 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
     let omniLight = createOmniLightAtPosition(position: SCNVector3(x: -10.0, y: 20, z: 10.0))
     self.rootNode.addChildNode(omniLight)
     
-    /* FOR LAB SESSION */
-    sharedMaterial = createSharedMaterial()
-    /* FOR LAB SESSION */
+    // TODO: Add code to initialize player here
     
-    // Create player
-    let playerGridPosition = levelData.coordinatesForGridPosition(column: playerGridCol, row: playerGridRow)
-    player = createPlayerAtPosition(position: SCNVector3(x: playerGridPosition.x, y: 0.1, z: playerGridPosition.z))
-    self.rootNode.addChildNode(player)
     
-    // Create the camera and make the camera always look at the player by using a lookat constraint
-    // The camera is added to the player node to ensure the camera always follows the player
-    camera = createCameraAtPosition(position: cameraOffsetFromPlayer)
-    camera.constraints = [SCNLookAtConstraint(target: player)]
-    player.addChildNode(camera)
+    // TODO: Add code to create camera here
+    
     
     // Create nodes for level
     level = createLevelAtPosition(position: SCNVector3Zero)
     self.rootNode.addChildNode(level)
     
-    // Create cars
-    setupCarSpawnNodes()
-    
     // Start the game
     switchToWaitingForFirstTap()
+    
   }
   
   
@@ -142,45 +117,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
     let swipeRightGesture = UISwipeGestureRecognizer(target: self, action: "handleSwipe:")
     swipeRightGesture.direction = UISwipeGestureRecognizerDirection.Right
     view.addGestureRecognizer(swipeRightGesture)
-  }
-  
-  
-  func setupCarSpawnNodes() {
-    // For each road, place car spawn node.
-    for row in 0..<levelData.data.rowCount() {
-      
-      let type = levelData.gameLevelDataTypeForGridPosition(column: 5, row: row)
-      
-      if type == GameLevelDataType.Road {
-        
-        // Determine if the car should start from the left of the right
-        let startCol = row % 2 == 0 ? 0 : levelData.data.columnCount() - 1
-        let moveDirection : Float = row % 2 == 0 ? 1.0 : -1.0
-        
-        // Determine the position of the node
-        var position = levelData.coordinatesForGridPosition(column: startCol, row: row)
-        position = SCNVector3(x: position.x, y: 0.15, z: position.z)
-        
-        // Create node
-        let spawnNode = SCNNode()
-        spawnNode.position = position
-        
-        // Create an action to make the node spawn cars
-        let spawnAction = SCNAction.runBlock({ node in
-          let car = self.createCarAtPosition(position: node.position, flipped: moveDirection > 0.0)
-          car.runAction(
-            SCNAction.sequence([SCNAction.moveBy(SCNVector3(x: moveDirection * self.levelData.gameLevelWidth(), y: 0.0, z: 0.0), duration: 10.0), SCNAction.removeFromParentNode()]))
-          self.rootNode.addChildNode(car)
-        })
-        // Will spawn a new car every 5 + (random time interval up to 5 seconds)
-        let delayAction = SCNAction.waitForDuration(5.0, withRange: 5.0)
-        
-        spawnNode.runAction(SCNAction.repeatActionForever(SCNAction.sequence([delayAction, spawnAction])))
-        
-        self.rootNode.addChildNode(spawnNode)
-      }
-      
-    }
   }
   
   
@@ -323,132 +259,17 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   }
   
   
-  /* func createPlayerAtPosition(#position: SCNVector3) -> SCNNode {
-    let rootNode = SCNNode()
-    rootNode.name = "Player"
-    rootNode.position = position
-    
-    // Create player model node
-    let playerGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0)
-    let playerMaterial = SCNMaterial()
-    playerMaterial.diffuse.contents = UIColor(red: 225.0/255.0, green: 225.0/255.0, blue: 225.0/255.0, alpha: 1.0)
-    playerMaterial.locksAmbientWithDiffuse = false
-    playerMaterial.specular.contents = UIColor.darkGrayColor()
-    playerMaterial.shininess = 0.5
-    playerGeometry.firstMaterial = playerMaterial
-    playerModelNode = SCNNode()
-    playerModelNode.geometry = playerGeometry
-    playerModelNode.name = "PlayerModel"
-    
-    // Create a physicsbody for collision detection
-    playerModelNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.Kinematic, shape: nil)
-    playerModelNode.physicsBody!.categoryBitMask = PhysicsCategory.Player
-    playerModelNode.physicsBody!.collisionBitMask = PhysicsCategory.Car
-    
-    rootNode.addChildNode(playerModelNode)
-    return rootNode
-  } */
-  
-  
-  /* FOR LAB SESSION */
-  func createPlayerAtPosition(#position: SCNVector3) -> SCNNode {
-    let rootNode = SCNNode()
-    rootNode.name = "Player"
-    rootNode.position = position
-    
-    // Create player model node
-    let playerGeometry = playerScene!.rootNode.childNodeWithName("Frog", recursively: true)!.geometry
-    let playerMaterial = SCNMaterial()
-    playerMaterial.diffuse.contents = UIColor(red: 225.0/255.0, green: 225.0/255.0, blue: 225.0/255.0, alpha: 1.0)
-    playerMaterial.locksAmbientWithDiffuse = false
-    playerMaterial.specular.contents = UIColor.darkGrayColor()
-    playerMaterial.shininess = 0.5
-    playerGeometry!.firstMaterial = sharedMaterial
-    playerModelNode = SCNNode()
-    playerModelNode.geometry = playerGeometry
-    playerModelNode.name = "PlayerModel"
-    
-    // Create a physicsbody for collision detection
-    playerModelNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.Kinematic, shape: nil)
-    playerModelNode.physicsBody!.categoryBitMask = PhysicsCategory.Player
-    playerModelNode.physicsBody!.collisionBitMask = PhysicsCategory.Car
-    
-    rootNode.addChildNode(playerModelNode)
-    return rootNode
-  }
-  /* FOR LAB SESSION */
-  
-  
-  /* FOR LAB SESSION */
-  func createSharedMaterial() -> SCNMaterial {
-    let material = SCNMaterial()
-    material.diffuse.contents = UIImage(named: "assets.scnassets/Textures/model_texture.tga")
-    material.locksAmbientWithDiffuse = false
-    material.specular.contents = UIColor.darkGrayColor()
-    material.shininess = 0.5
-    return material
-  }
-  /* FOR LAB SESSION */
-  
-  
-  /* func createCarAtPosition(#position: SCNVector3) -> SCNNode {
-    let carGeometry = SCNBox(width: 0.4, height: 0.3, length: 0.15, chamferRadius: 0.0)
-    
-    if sharedCarMaterial == nil {
-      sharedCarMaterial = SCNMaterial()
-      sharedCarMaterial.diffuse.contents = UIColor(red: 14.0/255.0, green: 149.0/255.0, blue: 204.0/255.0, alpha: 1.0)
-      sharedCarMaterial.locksAmbientWithDiffuse = false
-    }
-    carGeometry.firstMaterial = sharedCarMaterial
-    
-    let carNode = SCNNode(geometry: carGeometry)
-    carNode.name = "Car"
-    carNode.position = position
-    
-    // Create a physicsbody for collision detection
-    carNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.Kinematic, shape: nil)
-    carNode.physicsBody!.categoryBitMask = PhysicsCategory.Car
-    carNode.physicsBody!.collisionBitMask = PhysicsCategory.Player
-    
-    return carNode
-  } */
-  
-  
-  /* FOR CHALLENGE SESSION */
-  func createCarAtPosition(#position: SCNVector3, flipped: Bool) -> SCNNode {
-    let carGeometry = carScene!.rootNode.childNodeWithName("Car", recursively: true)!.geometry
-    carGeometry!.firstMaterial = sharedMaterial
-    
-    let carNode = SCNNode(geometry: carGeometry!)
-    carNode.name = "Car"
-    carNode.position = position
-    
-    if flipped {
-      carNode.rotation = SCNVector4(x: 0.0, y: 1.0, z: 0.0, w: 3.1415)
-    }
-    
-    // Create a physicsbody for collision detection
-    carNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.Kinematic, shape: nil)
-    carNode.physicsBody!.categoryBitMask = PhysicsCategory.Car
-    carNode.physicsBody!.collisionBitMask = PhysicsCategory.Player
-    
-    return carNode
-  }
-  /* FOR CHALLENGE SESSION */
-  
-  
   // MARK: Game Play
   
-  
   func didPlayerReachEndOfLevel() -> Bool {
-    return playerGridRow == levelData.data.rowCount() - 7
+    //return playerGridRow == levelData.data.rowCount() - 7
+    return false
   }
   
   
   // MARK: Game State
   
   func switchToWaitingForFirstTap() {
-    println("GameState changed to WaitingForFirstTap")
     gameState = GameState.WaitingForFirstTap
     if let overlay = view.overlaySKScene {
       // Remove black node
@@ -477,7 +298,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   
   
   func switchToPlaying() {
-    println("GameState changed to Playing")
     gameState = GameState.Playing
     if let overlay = view.overlaySKScene {
       // Remove tutorial
@@ -491,7 +311,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   
   
   func switchToGameOver() {
-    println("GameState changed to GameOver")
     gameState = GameState.GameOver
     if let overlay = view.overlaySKScene {
       let gameOverLabel = SKLabelNode(fontNamed: "Early-GameBoy")
@@ -514,7 +333,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   }
   
   func switchToRestartLevel() {
-    println("GameState changed to RestartLevel")
     gameState = GameState.RestartLevel
     if let overlay = view.overlaySKScene {
       overlay.enumerateChildNodesWithName("GameOver", usingBlock: { node, stop in
@@ -543,13 +361,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   // MARK: Delegates
   
   func renderer(aRenderer: SCNSceneRenderer, didRenderScene scene: SCNScene, atTime time: NSTimeInterval) {
-    // Timing
-    if previousTime == 0.0 {
-      previousTime = time
-    }
-    deltaTime = time - previousTime
-    previousTime = time
-    
     if didPlayerReachEndOfLevel() && gameState == GameState.Playing {
       // player completed the level
       switchToGameOver()
@@ -569,7 +380,7 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
   
   func handleTap(gesture: UIGestureRecognizer) {
     if let tapGesture = gesture as? UITapGestureRecognizer {
-      movePlayerInDirection(MoveDirection.Forward)
+      // TODO: Add code to do movement here
     }
   }
   
@@ -579,19 +390,19 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
     if let swipeGesture = gesture as? UISwipeGestureRecognizer {
       switch swipeGesture.direction {
       case UISwipeGestureRecognizerDirection.Up:
-        movePlayerInDirection(MoveDirection.Forward)
+        // TODO: Add code to do movement here
         break
         
       case UISwipeGestureRecognizerDirection.Down:
-        movePlayerInDirection(MoveDirection.Backward)
+        // TODO: Add code to do movement here
         break
         
       case UISwipeGestureRecognizerDirection.Left:
-        movePlayerInDirection(MoveDirection.Left)
+        // TODO: Add code to do movement here
         break
         
       case UISwipeGestureRecognizerDirection.Right:
-        movePlayerInDirection(MoveDirection.Right)
+        // TODO: Add code to do movement here
         break
         
       default:
@@ -600,90 +411,6 @@ class GameScene : SCNScene, SCNSceneRendererDelegate, SCNPhysicsContactDelegate 
     }
   }
   
-  func movePlayerInDirection(direction: MoveDirection) {
-    
-    switch gameState {
-    case GameState.WaitingForFirstTap:
-      
-      // Start playing
-      switchToPlaying()
-      movePlayerInDirection(direction)
-      break
-      
-    case GameState.Playing:
-      
-      // Determine if the new position is a valid position
-      var newPlayerGridCol = playerGridCol
-      var newPlayerGridRow = playerGridRow
-      
-      switch direction {
-      case .Forward:
-        newPlayerGridRow += 1
-        break;
-      case .Backward:
-        newPlayerGridRow -= 1
-        break
-      case .Left:
-        newPlayerGridCol -= 1
-        break
-      case .Right:
-        newPlayerGridCol += 1
-      }
-      
-      // Determine the type of tile at new position
-      let type = levelData.gameLevelDataTypeForGridPosition(column: newPlayerGridCol, row: newPlayerGridRow)
-      
-      if type == GameLevelDataType.Invalid || type == GameLevelDataType.Obstacle {
-        // Invalid - do not move
-        // println("Invalid move")
-        
-      } else {
-        // Valid - move
-        // println("Valid move to \(newPlayerGridCol), \(newPlayerGridRow)")
-        
-        playerGridCol = newPlayerGridCol
-        playerGridRow = newPlayerGridRow
-        
-        // Move the player to new position
-        var newPlayerPosition = levelData.coordinatesForGridPosition(column: playerGridCol, row: playerGridRow)
-        newPlayerPosition = SCNVector3(x: newPlayerPosition.x, y: 0.1, z: newPlayerPosition.z)
-        
-        // Move the player using an action
-        let moveAction = SCNAction.moveTo(newPlayerPosition, duration: 0.2)
-        let jumpUpAction = SCNAction.moveBy(SCNVector3(x: 0.0, y: 0.2, z: 0.0), duration: 0.1)
-        jumpUpAction.timingMode = SCNActionTimingMode.EaseInEaseOut
-        let jumpDownAction = SCNAction.moveBy(SCNVector3(x: 0.0, y: -0.2, z: 0.0), duration: 0.1)
-        jumpDownAction.timingMode = SCNActionTimingMode.EaseInEaseOut
-        let jumpAction = SCNAction.sequence([jumpUpAction, jumpDownAction])
-        
-        // Play the action
-        player.runAction(moveAction)
-        playerModelNode.runAction(jumpAction)
-        
-        // Play jump sound
-        if let overlay = view.overlaySKScene {
-          overlay.runAction(soundJump)
-        }
-      }
-      
-      break
-      
-    case GameState.GameOver:
-      
-      // Switch to tutorial
-      switchToRestartLevel()
-      break
-      
-    case GameState.RestartLevel:
-      
-      // Switch to new level
-      // switchToWaitingForFirstTap()
-      break
-      
-    default:
-      break
-    }
-    
-  }
+  // TODO: Add code for player movement here
   
 }
