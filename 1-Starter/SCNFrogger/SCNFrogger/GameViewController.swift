7cb96694639d68200152cdf4595efee7019b2ebb
Kim Pedersen <dretox@gmail.com>
2014-12-02 11:36:43 +0100

    Initial Commit

---
+++ 1-Starter/SCNFrogger/SCNFrogger/GameViewController.swift
@@ -0,0 +1,131 @@
+//
+//  GameViewController.swift
+//  SCNFrogger
+//
+//  Created by Kim Pedersen on 02/12/14.
+//  Copyright (c) 2014 RWDevCon. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.dae")!
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = SCNLightTypeOmni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLightTypeAmbient
+        ambientLightNode.light!.color = UIColor.darkGrayColor()
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the ship node
+        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
+        
+        // animate the 3d object
+        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        
+        // retrieve the SCNView
+        let scnView = self.view as SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.blackColor()
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        let gestureRecognizers = NSMutableArray()
+        gestureRecognizers.addObject(tapGesture)
+        if let existingGestureRecognizers = scnView.gestureRecognizers {
+            gestureRecognizers.addObjectsFromArray(existingGestureRecognizers)
+        }
+        scnView.gestureRecognizers = gestureRecognizers
+    }
+    
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.locationInView(scnView)
+        if let hitResults = scnView.hitTest(p, options: nil) {
+            // check that we clicked on at least one object
+            if hitResults.count > 0 {
+                // retrieved the first clicked object
+                let result: AnyObject! = hitResults[0]
+                
+                // get its material
+                let material = result.node!.geometry!.firstMaterial!
+                
+                // highlight it
+                SCNTransaction.begin()
+                SCNTransaction.setAnimationDuration(0.5)
+                
+                // on completion - unhighlight
+                SCNTransaction.setCompletionBlock {
+                    SCNTransaction.begin()
+                    SCNTransaction.setAnimationDuration(0.5)
+                    
+                    material.emission.contents = UIColor.blackColor()
+                    
+                    SCNTransaction.commit()
+                }
+                
+                material.emission.contents = UIColor.redColor()
+                
+                SCNTransaction.commit()
+            }
+        }
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
---
+++ 5-Conclusion/RWDevConSceneKitFinalTests/RWDevConSceneKitFinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  RWDevConSceneKitFinalTests.swift
+//  RWDevConSceneKitFinalTests
+//
+//  Created by Kim Pedersen on 24/11/14.
+//  Copyright (c) 2014 RWDevCon. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RWDevConSceneKitFinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
